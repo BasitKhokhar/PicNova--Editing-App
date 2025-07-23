11a8467dd855252e2503903d0bc65b3b081a1aaa
BasitKhokhar <127882915+BasitKhokhar@users.noreply.github.com>
2025-07-23 16:36:57 +0500

    updates

--- Components/User/UserScreen.jsx
+++ Components/User/UserScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, RefreshControl } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from "@react-navigation/native";
 import SocialIconsRow from "./SocialIconsRow";
 import Loader from "../Loader/Loader";
@@ -73,7 +74,6 @@ const UserScreen = () => {
           <View style={styles.profileContainer}>
             {/* Profile Header */}
             <View style={styles.header}>
-              <Text style={styles.title}>{userData.name}</Text>
               <View style={styles.imageContainer}>
                 {userImage && typeof userImage === 'string' && userImage.startsWith('http') ? (
                   <Image
@@ -88,9 +88,85 @@ const UserScreen = () => {
                   <View style={styles.defaultProfileCircle} />
                 )}
               </View>
+              <View style={styles.headingscontianer}>
+                <Text style={styles.title}>{userData.name}</Text>
+                <Text style={styles.email}>{userData.email}</Text>
+              </View>
+
+
             </View>
 
-            {/* Navigation Sections */}
+            <View style={styles.sectionHeader}>
+              <Text style={styles.heading}>General</Text>
+              <View style={styles.line} />
+            </View>
+            
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('AccountDetail', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="person" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>Account Detail</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('AccountDetail', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="person" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>Account Detail</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('AccountDetail', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="person" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>Account Detail</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.heading}>About</Text>
+              <View style={styles.line} />
+            </View>
+           <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('about', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="info" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>About</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('CustomerSupport', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="support-agent" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>Customer Support</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('faq', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="question-answer" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>FAQs</Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.section} onPress={() => navigation.navigate('Logout', { userData })}>
+              <View style={styles.leftContent}>
+                <Icon name="logout" size={24} color="#555" style={styles.icon} />
+                <Text style={styles.sectionText}>Logout </Text>
+              </View>
+              <View><Icon name="chevron-right" size={24} color="#888" /></View>
+            </TouchableOpacity>
+
+
+
+
+
+            {/* <TouchableOpacity style={styles.section} onPress={() => navigation.navigate("PaymentScreen", { userData })}>
+              <Text style={styles.sectionText}>Account Detail</Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.section} onPress={() => navigation.navigate("AccountDetail", { userData })}>
               <Text style={styles.sectionText}>Account Detail</Text>
             </TouchableOpacity>
@@ -105,7 +181,7 @@ const UserScreen = () => {
             </TouchableOpacity>
             <TouchableOpacity style={[styles.section, styles.logout]} onPress={() => navigation.navigate("Logout")}>
               <Text style={styles.sectionText}>Logout</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
 
             {/* <View style={styles.iconscontainer}>
             <SocialIconsRow />
@@ -122,7 +198,7 @@ const UserScreen = () => {
 
 const styles = StyleSheet.create({
   maincontainer: {
-    backgroundColor: '#1A1A1A',
+    backgroundColor: '#8b3dff',
     paddingTop: 30,
     width: '100%',
     height: '100%',
@@ -150,28 +226,73 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
+    gap: 10,
     marginTop: 15,
     marginBottom: 50,
-    justifyContent: "space-between",
+    // justifyContent: "space-between",
     width: "100%",
     borderRadius: 10,
   },
+  headingscontianer: {
+    display: 'flex',
+    flexDirection: 'column',
+
+  },
   title: {
-    fontSize: 26,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#8b3dff"
+  },
+  email: {
+    fontSize: 14,
     fontWeight: "bold",
     color: "#333"
   },
-  section: {
-    width: "100%",
-    paddingVertical: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-    alignItems: "flex-start"
+  sectionHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
   },
-  sectionText: {
+  heading: {
     fontSize: 18,
-    color: "#333"
+    fontWeight: 'bold',
+    color: '#8b3dff',
+    marginRight: 8,
   },
+  line: {
+    flex: 1,
+    height: 2,
+    backgroundColor: '#8b3dff',
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  leftContent: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+  },
+
+  sectionText: {
+    fontSize: 20,
+    color: '#333',
+  },
+  // section: {
+  //   width: "100%",
+  //   paddingVertical: 15,
+  //   borderBottomWidth: 1,
+  //   borderBottomColor: "#ddd",
+  //   alignItems: "flex-start"
+  // },
+  // sectionText: {
+  //   fontSize: 18,
+  //   color: "#333"
+  // },
   logout: {
     borderBottomWidth: 0
   },
